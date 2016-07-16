80523e311b0c8c4f307cdb05125044ed4e1d4abf
Lingnan Zeng <zenglingnan@gmail.com>
2016-07-16 12:59:21 +0800

    Fix path

--- sock-sort.sh
+++ sock-sort.sh
@@ -32,7 +32,14 @@
 echo "pre-commit pysock script start"
 
 temp_file_name="sock_temp_file_which_will_be_deleted_automatically"
-script_name="pysock/sock.py"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+script_name="$DIR/pysock/sock.py"
 
 ######## Getting staged files paths and dumping to file
 git diff --name-only --cached >> $temp_file_name
